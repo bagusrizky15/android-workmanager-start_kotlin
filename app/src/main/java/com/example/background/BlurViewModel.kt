13e0790cb6f000f93bc42b76e9c5f7ff4bd4002a
mohamad.15@students.amikom.ac.id <mohamad.15@students.amikom.ac.id>
2022-12-25 00:39:43 +0700

    Update BlurViewModel.kt

--- app/src/main/java/com/example/background/BlurViewModel.kt
+++ app/src/main/java/com/example/background/BlurViewModel.kt
@@ -27,6 +27,8 @@ import androidx.work.OneTimeWorkRequest
 import androidx.work.OneTimeWorkRequestBuilder
 import androidx.work.WorkManager
 import com.example.background.workers.BlurWorker
+import com.example.background.workers.CleanupWorker
+import com.example.background.workers.SaveImageToFileWorker
 
 
 class BlurViewModel(application: Application) : ViewModel() {
@@ -43,10 +45,23 @@ class BlurViewModel(application: Application) : ViewModel() {
      * @param blurLevel The amount to blur the image
      */
     internal fun applyBlur(blurLevel: Int) {
-        val blurRequest = OneTimeWorkRequestBuilder<BlurWorker>()
+        var continuation = workManager
+            .beginWith(OneTimeWorkRequest
+                .from(CleanupWorker::class.java))
+
+        val blurRequest = OneTimeWorkRequest
+            .Builder(BlurWorker::class.java)
             .setInputData(createInputDataForUri())
             .build()
-        workManager.enqueue(blurRequest)
+
+        continuation = continuation.then(blurRequest)
+
+        val save = OneTimeWorkRequest
+            .Builder(SaveImageToFileWorker::class.java).build()
+
+        continuation = continuation.then(save)
+
+        continuation.enqueue()
     }
 
     private fun uriOrNull(uriString: String?): Uri? {
